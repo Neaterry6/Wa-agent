ee5851caa899f462ff130082b514db3d694cb4f8
Heisbroken 🎀🎧✍️ <189724328+Neaterry6@users.noreply.github.com>
2026-05-19 10:53:09 +0100

    Fix archiver crash and add multimodal chat UI

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,7 @@ import { exec } from "child_process";
 import { promisify } from "util";
 import fs from "fs";
 import path from "path";
-import { create as archiver } from "archiver";
+import archiver from "archiver";
 import AdmZip from "adm-zip";
 
 
